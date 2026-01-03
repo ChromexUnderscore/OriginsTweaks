577c315bcd44dce86510facfee9f6836923d0a90
Chromex <87236987+ChromexUnderscore@users.noreply.github.com>
2026-01-03 15:13:03 +0100

    Refactor Client Assets and Main Entrypoint
    Move Client Assets to Main Assets
    Rename ``com.chromexunderscore.originstweaks.OriginsTweaks`` to ``com.chromexunderscore.originstweaks.Main``

--- src/main/java/com/chromexunderscore/originstweaks/Originstweaks.java
+++ src/main/java/com/chromexunderscore/originstweaks/Originstweaks.java
@@ -7,18 +7,11 @@ import org.slf4j.LoggerFactory;
 
 public class OriginsTweaks implements ModInitializer {
 	public static final String MOD_ID = "OriginsTweaks";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("For the best OriginsTweaks experience, enable flying in the server.properties and set \"mixin.entity.collisions.suffocation=false\" in lithium.properties when using lithium.");
+		LOGGER.info(
+				"For the best OriginsTweaks experience, enable flying in the server.properties and set \"mixin.entity.collisions.suffocation=false\" in lithium.properties when using lithium.");
 	}
 }
